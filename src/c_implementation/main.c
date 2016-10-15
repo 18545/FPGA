19ddfebdba114d28e94960a959ee191dedeb206a
rmaratos <rmaratos@andrew.cmu.edu>
2016-10-15 16:13:57 -0400

    separate out png functions

--- src/c_implementation/main.c
+++ src/c_implementation/main.c
@@ -1,185 +1,133 @@
-/*
- * Copyright 2002-2010 Guillaume Cottenceau.
- *
- * This software may be freely redistributed under the terms
- * of the X11 license.
- *
- */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-
-#define PNG_DEBUG 3
+#include <stdbool.h>
+#include "pnglib.h"
 #include <png.h>
 
-void abort_(const char * s, ...)
+#define PNG_DEBUG 3
+#define X_MIN 50
+#define X_MAX 250
+#define Y_MIN 50
+#define Y_MAX 250
+
+
+extern int width, height;
+
+void blue(png_bytep * row_pointers)
 {
-        va_list args;
-        va_start(args, s);
-        vfprintf(stderr, s, args);
-        fprintf(stderr, "\n");
-        va_end(args);
-        abort();
+  int x, y;
+  for (y=0; y<height; y++) {
+          png_byte* row = row_pointers[y];
+          for (x=0; x<width; x++) {
+                  png_byte* ptr = &(row[x*4]);
+                  printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
+                         x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
+
+                  /* set red value to 0 and green value to the blue one */
+                  ptr[0] = 0;
+                  ptr[1] = ptr[2];
+          }
+  }
 }
 
-int x, y;
-
-int width, height;
-png_byte color_type;
-png_byte bit_depth;
-
-png_structp png_ptr;
-png_infop info_ptr;
-int number_of_passes;
-png_bytep * row_pointers;
-
-void read_png_file(char* file_name)
-{
-        char header[8];    // 8 is the maximum size that can be checked
-
-        /* open file and test for it being a png */
-        FILE *fp = fopen(file_name, "rb");
-        if (!fp)
-                abort_("[read_png_file] File %s could not be opened for reading", file_name);
-        fread(header, 1, 8, fp);
-        if (png_sig_cmp(header, 0, 8))
-                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
-
-
-        /* initialize stuff */
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-        if (!png_ptr)
-                abort_("[read_png_file] png_create_read_struct failed");
-
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr)
-                abort_("[read_png_file] png_create_info_struct failed");
-
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during init_io");
-
-        png_init_io(png_ptr, fp);
-        png_set_sig_bytes(png_ptr, 8);
-
-        png_read_info(png_ptr, info_ptr);
-
-        width = png_get_image_width(png_ptr, info_ptr);
-        height = png_get_image_height(png_ptr, info_ptr);
-        color_type = png_get_color_type(png_ptr, info_ptr);
-        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-        number_of_passes = png_set_interlace_handling(png_ptr);
-        png_read_update_info(png_ptr, info_ptr);
-
-
-        /* read file */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during read_image");
-
-        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        for (y=0; y<height; y++)
-                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
-
-        png_read_image(png_ptr, row_pointers);
-
-        fclose(fp);
+bool in_box(int x, int y) {
+  return ((x == X_MIN || x == X_MAX) && y >= Y_MIN && y <= Y_MAX) || \
+          ((y == Y_MIN || y == Y_MAX) && x >= X_MIN && x <= X_MAX);
 }
 
-
-void write_png_file(char* file_name)
+void box(png_bytep * row_pointers)
 {
-        /* create file */
-        FILE *fp = fopen(file_name, "wb");
-        if (!fp)
-                abort_("[write_png_file] File %s could not be opened for writing", file_name);
+  int x, y;
 
+  for (y=0; y<height; y++) {
+          png_byte* row = row_pointers[y];
+          for (x=0; x<width; x++) {
+                  png_byte* ptr = &(row[x*4]);
+                  printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
+                         x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
 
-        /* initialize stuff */
-        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+                  /* set red value to 0 and green value to the blue one */
+                  if (in_box(x, y)) {
+                    // printf("Setting value to 0\n");
+                    ptr[0] = 0;
+                    ptr[1] = 0;
+                    ptr[2] = 0;
+                    ptr[3] = 0;
+                  }
 
-        if (!png_ptr)
-                abort_("[write_png_file] png_create_write_struct failed");
-
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr)
-                abort_("[write_png_file] png_create_info_struct failed");
-
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during init_io");
-
-        png_init_io(png_ptr, fp);
-
-
-        /* write header */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during writing header");
-
-        png_set_IHDR(png_ptr, info_ptr, width, height,
-                     bit_depth, color_type, PNG_INTERLACE_NONE,
-                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-        png_write_info(png_ptr, info_ptr);
-
-
-        /* write bytes */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during writing bytes");
-
-        png_write_image(png_ptr, row_pointers);
-
-
-        /* end write */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during end of write");
-
-        png_write_end(png_ptr, NULL);
-
-        /* cleanup heap allocation */
-        for (y=0; y<height; y++)
-                free(row_pointers[y]);
-        free(row_pointers);
-
-        fclose(fp);
+          }
+  }
 }
-
-
-void process_file(void)
-{
-        if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGB)
-                abort_("[process_file] input file is PNG_COLOR_TYPE_RGB but must be PNG_COLOR_TYPE_RGBA "
-                       "(lacks the alpha channel)");
-
-        if (png_get_color_type(png_ptr, info_ptr) != PNG_COLOR_TYPE_RGBA)
-                abort_("[process_file] color_type of input file must be PNG_COLOR_TYPE_RGBA (%d) (is %d)",
-                       PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
-
-        for (y=0; y<height; y++) {
-                png_byte* row = row_pointers[y];
-                for (x=0; x<width; x++) {
-                        png_byte* ptr = &(row[x*4]);
-                        printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
-                               x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
-
-                        /* set red value to 0 and green value to the blue one */
-                        ptr[0] = 0;
-                        ptr[1] = ptr[2];
-                }
-        }
-}
-
+//
+// void sobel_filtering( )
+//      /* Spatial filtering of image data */
+//      /* Sobel filter (horizontal differentiation */
+//      /* Input: image1[y][x] ---- Outout: image2[y][x] */
+// {
+//   /* Definition of Sobel filter in horizontal direction */
+//   int weight[3][3] = {{ -1,  0,  1 },
+//           { -2,  0,  2 },
+//           { -1,  0,  1 }};
+//   double pixel_value;
+//   double min, max;
+//   int x, y, i, j;  /* Loop variable */
+//
+//   /* Maximum values calculation after filtering*/
+//   printf("Now, filtering of input image is performed\n\n");
+//   min = DBL_MAX;
+//   max = -DBL_MAX;
+//   for (y = 1; y < y_size1 - 1; y++) {
+//     for (x = 1; x < x_size1 - 1; x++) {
+//       pixel_value = 0.0;
+//       for (j = -1; j <= 1; j++) {
+//       for (i = -1; i <= 1; i++) {
+//         pixel_value += weight[j + 1][i + 1] * image1[y + j][x + i];
+//       }
+//       }
+//       if (pixel_value < min) min = pixel_value;
+//       if (pixel_value > max) max = pixel_value;
+//     }
+//   }
+//   if ((int)(max - min) == 0) {
+//     printf("Nothing exists!!!\n\n");
+//     exit(1);
+//   }
+//
+//   /* Initialization of image2[y][x] */
+//   x_size2 = x_size1;
+//   y_size2 = y_size1;
+//   for (y = 0; y < y_size2; y++) {
+//     for (x = 0; x < x_size2; x++) {
+//       image2[y][x] = 0;
+//     }
+//   }
+//   /* Generation of image2 after linear transformtion */
+//   for (y = 1; y < y_size1 - 1; y++) {
+//     for (x = 1; x < x_size1 - 1; x++) {
+//       pixel_value = 0.0;
+//       for (j = -1; j <= 1; j++) {
+//       for (i = -1; i <= 1; i++) {
+//         pixel_value += weight[j + 1][i + 1] * image1[y + j][x + i];
+//       }
+//       }
+//       pixel_value = MAX_BRIGHTNESS * (pixel_value - min) / (max - min);
+//       image2[y][x] = (unsigned char)pixel_value;
+//     }
+//   }
+// }
 
 int main(int argc, char **argv)
 {
-        if (argc != 3)
-                abort_("Usage: program_name <file_in> <file_out>");
+  if (argc != 3)
+    abort_("Usage: program_name <file_in> <file_out>");
 
-        read_png_file(argv[1]);
-        process_file();
-        write_png_file(argv[2]);
+  // READ IN PNG FILE
+  png_bytep *row_pointers = read_png_file(argv[1]);
 
-        return 0;
+  // ADD MORE EFFECTS HERE
+  blue(row_pointers);
+  box(row_pointers);
+
+  // WRITE OUT EDITED PNG FILE
+  write_png_file(row_pointers, argv[2]);
+  return 0;
 }
